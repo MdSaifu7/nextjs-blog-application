ff77b2390ebb54c577f880dec425603534b4a1aa
MdSaifu7 <mdsaifkhanahmad@gmail.com>
2026-06-23 08:13:08 +0530

    Small-fix

--- proxy.ts
+++ proxy.ts
@@ -2,18 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function proxy(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
+  const sessionCookie = getSessionCookie(request, {
+    cookiePrefix: "better-auth", // must match your Better Auth config
+    https: process.env.NODE_ENV === "production", // ← ensures secure cookie is read
+  });
 
-  // THIS IS NOT SECURE!
-  // This is the recommended approach to optimistically redirect users
-  // We recommend handling auth checks in each page/route
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/auth/sign-in", request.url));
   }
 
   return NextResponse.next();
 }
-
 export const config = {
   matcher: ["/blogs", "/create"], // Specify the routes the middleware applies to
 };
