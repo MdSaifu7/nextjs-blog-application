0089bae344c9bafe8a82d2b243b7f805e90351c8
MdSaifu7 <mdsaifkhanahmad@gmail.com>
2026-06-23 07:46:23 +0530

    Small-fix

--- convex/auth.ts
+++ convex/auth.ts
@@ -15,6 +15,10 @@ export const authComponent = createClient<DataModel>(components.betterAuth);
 export const createAuth = (ctx: GenericCtx<DataModel>) => {
   return betterAuth({
     baseURL: siteUrl,
+    trustedOrigins: [
+      "http://localhost:3000",
+      process.env.TRUSTED_ORIGIN!, // ← Add this
+    ],
     database: authComponent.adapter(ctx),
     // Configure simple, non-verified email/password to get started
     emailAndPassword: {
